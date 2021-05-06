9287977e3b87dd1c0066a3c304a57794b8038b7a
ARYAman25aug <77323196+ARYAman25aug@users.noreply.github.com>
2021-05-06 20:37:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,20 @@
 var balloon, background;
 function preload() {
-  backgroundImg = loadImage("images/1.png")
-  balloonImage = loadAnimation("images/2.png", "images/3.png", "images/4.png");
+  backgroundImg = loadImage("1.png")
+  balloonImage = loadAnimation("2.png", "3.png", "4.png");
 }
 
 function setup(){
    database = firebase.database();
    console.log(database);
-    createCanvas(500,500);
+    createCanvas(1500,700);
 
 balloon = createSprite (100,400, 20, 20)
   balloon.addAnimation("balloon", balloonImage);
   balloon.scale = 0.4;
 
-
+  var balloonPos=database.ref('balloon/position')
+  balloonPos.on("value",readPos)
 }
 
 function draw(){
@@ -21,20 +22,32 @@ function draw(){
     background(backgroundImg);
   
         if(keyDown(LEFT_ARROW)){
+            updatePos(-10,0)
             balloon.x = balloon.x - 10;
         }
         else if(keyDown(RIGHT_ARROW)){
+            updatePos(10,0)
             balloon.x = balloon.x + 10;
         }
         else if(keyDown(UP_ARROW)){
+            updatePos(0,-10)
             balloon.y = balloon.y - 10;
         }
         else if(keyDown(DOWN_ARROW)){
+            updatePos(0,10)
              balloon.y = balloon.y + 10;
         }
         drawSprites();
     }
-
+function updatePos(x,y){
+database.ref('balloon/position').set({
+    'x':height.x+x,
+    'y':height.y+y
+})
+}
     
-
-
+function readPos(data){
+    height=data.val();
+    balloon.x=height.x;
+    balloon.y=height.y
+}
